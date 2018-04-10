b11338fe9b03122e0c7e50746668a1faa04c32c9
Dario Breitenstein <chdabre@users.noreply.github.com>
2018-04-10 08:56:50 +0200

    Implement Timeline

--- webinterface/js/app.js
+++ webinterface/js/app.js
@@ -16,15 +16,21 @@ var app = new Vue({
     data: {
         image: [],
         images: [],
+        timelines: [{name: "Timeline 1", frames: [], speed: 100, repeat: true}],
+        timeline: null,
+        newTimeline: {name: "", frames: [], speed: 100, repeat: true},
         pixels: [],
         newPixel: {"ip":"192.168.1.0", "name": "Name", "intensity": 8},
-        instantUpdate: true,
+        instantUpdate: false,
         lastPixel: null,
         imageIndex: null,
+        imageSource: null,
         hex: "0000000000000000",
         importData: "",
+        importTimeline: "",
         message: "",
-        msgRepeat: false
+        msgRepeat: false,
+        previewIntervalId: null,
     },
     methods:{
         clearImage: function () {
@@ -38,11 +44,16 @@ var app = new Vue({
         },
         deleteImage: function() {
             if(this.imageIndex !== null) {
-                this.images.splice(this.imageIndex, 1)
+                if(this.imageSource == "saved"){
+                    this.images.splice(this.imageIndex, 1)
+                }else if (this.imageSource = "timeline") {
+                    this.timeline.frames.splice(this.imageIndex, 1)
+                }
                 this.clearImage()
             }
         },
         getImage: function (index) {
+            this.imageSource = "saved"
             this.imageIndex = index
             this.image = this.images[index]
         },
@@ -83,6 +94,82 @@ var app = new Vue({
             console.log(importData)
             this.images = this.images.concat(importData)
             this.importData = ""
+        },
+        importTimelines: function () {
+            var importTimelines = JSON.parse(this.importTimeline)
+
+            for(i in importTimelines){
+                importTimelines[i].frames = importTimelines[i].frames.map(function (frame) {
+                    return hextoBytes(frame)
+                })
+            }
+
+            console.log(importTimelines)
+            this.timelines = this.timelines.concat(importTimelines)
+            this.importTimeline = ""
+        },
+        addToTimeline: function () {
+            if(parseInt(this.image.join(''), 2) > 0) {
+                this.timeline.frames.splice(this.imageIndex, 0, this.image.slice());
+                this.imageIndex++
+                this.getImageFromTimeline(this.imageIndex)
+            }
+        },
+        getImageFromTimeline: function (index) {
+            this.imageSource = "timeline"
+            this.imageIndex = index
+            this.image = this.timeline.frames[index]
+        },
+        playPreview: function () {
+            this.previewIntervalId = setInterval(() => {
+                this.imageIndex++
+
+                if(this.imageIndex >= this.timeline.frames.length) {
+                    if(this.timeline.repeat) {
+                        this.imageIndex = 0
+                    } else {
+                        this.stopPreview()
+                        return
+                    }
+                }
+
+                this.getImageFromTimeline(this.imageIndex)
+
+            }, this.timeline.speed)
+        },
+        stopPreview: function () {
+            clearInterval(this.previewIntervalId)
+            this.previewIntervalId = null
+        },
+        sendTimeline: function () {
+            var hexFrames = this.timeline.frames.map(function (frame) {
+                return bytesToHex(frame)
+            })
+
+            url = 'http://' + this.lastPixel.ip + '/?animation=' + hexFrames.join('') + '&speed=' + this.timeline.speed
+            if (this.timeline.repeat) url += "&repeat=true"
+
+            console.log("REQUEST: " + url)
+
+            this.$http.get(url).then(response => {
+                console.log("SUCCESS", response)
+            }, response => {
+                console.log("ERROR", response)
+            })
+        },
+        getTimeLine: function (timeline) {
+            this.timeline = timeline
+            this.imageIndex = 0
+        },
+        createTimeline: function (){
+            if(this.newTimeline.name.length > 0){
+                this.timelines.push(this.newTimeline)
+                this.timeline = this.newTimeline
+            }
+        },
+        deleteTimeline: function () {
+            this.timelines.splice(this.timelines.indexOf(this.timeline), 1)
+            this.timeline = this.timelines[0]
         }
     },
     computed: {
@@ -92,6 +179,17 @@ var app = new Vue({
             })
 
             return JSON.stringify(imagesHex)
+        },
+        stringTimelines: function () {
+            var timelines = JSON.parse(JSON.stringify(this.timelines))
+            console.log(timelines)
+            for (i in timelines) {
+              timelines[i].frames = timelines[i].frames.map(function (value) {
+                    return bytesToHex(value)
+                })
+            }
+
+            return JSON.stringify(timelines)
         }
     },
     watch: {
@@ -107,6 +205,12 @@ var app = new Vue({
           },
           deep: true
       },
+      timelines: {
+          handler: function () {
+              localStorage.setItem('PixelTimelineStorage', JSON.stringify(this.timelines))
+          },
+          deep: true
+      },
       pixels: {
           handler: function () {
               if(this.lastPixel !== null) this.sendImage(this.lastPixel)
@@ -119,7 +223,13 @@ var app = new Vue({
         this.image = new Array(64).fill(0)
         var localImages = JSON.parse(localStorage.getItem('PixelImageStorage'))  
         this.images = localImages !== null ? localImages : []
-        
+
+        var localTimelines = JSON.parse(localStorage.getItem('PixelTimelineStorage'))
+        if (localTimelines){
+             this.timelines = localTimelines
+        }
+        this.timeline = this.timelines[0]
+
         var localPixels = JSON.parse(localStorage.getItem('PixelStorage'))  
         this.pixels = localPixels !== null ? localPixels : [] 
         
